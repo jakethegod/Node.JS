53bb2c77ea32c840ed2a57ddd358c307f465ba24
Kirill Keker <k.keker@globus-ltd.com>
2017-04-07 22:29:46 +0300

    add App files

---
+++ Server.js
@@ -0,0 +1,16 @@
+var rpc = require('json-rpc2')
+var server = rpc.Server.$create({
+    'websocket': true,
+    'headers': {
+        'Access-Control-Allow-Origin': '*'
+    }
+})
+
+function add(args, opt, callback) {
+    callback(null, args[0] + args[1])
+}
+
+server.expose('add', add)
+server.listen(8000, 'localhost')
+
+console.log("Server is started on http://localhost:8000/")
