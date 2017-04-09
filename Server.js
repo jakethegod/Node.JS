548c07ffeee57000ee723cf9bcd4278279a820a6
Kirill Keker <k.keker@globus-ltd.com>
2017-04-09 20:15:53 +0300

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,33 +1,17 @@
 const rpc = require('json-rpc2')
-const cluster = require('cluster')
-const numCPUs = require('os').cpus().length
 
-if (cluster.isMaster) {
-
-    console.log(`Master ${process.pid} is running`)
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
+var server = rpc.Server.$create({
+    'websocket': true,
+    'headers': {
+        'Access-Control-Allow-Origin': '*'
     }
+})
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`)
-    })
-
-} else {
-
-    var server = rpc.Server.$create({
-        'websocket': true,
-        'headers': {
-            'Access-Control-Allow-Origin': '*'
-        }
-    })
-
-    function add(args, opt, callback) {
-        callback(null, args[0] + args[1])
-    }
-
-    server.expose('add', add)
-    server.listen(8000, 'localhost')
-
-    console.log(`Worker ${process.pid} started on http://localhost:8000/`)
+function add(args, opt, callback) {
+    callback(null, args[0] + args[1])
 }
+
+server.expose('add', add)
+server.listen(8000, 'localhost')
+
+console.log(Server started on http://localhost:8000/`)
