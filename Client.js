78c9f66467dadf1bf0db20362b37758b61c57bb3
Andrey Korolev <brainstorm2004@ya.ru>
2017-04-10 23:50:51 +0300

    Написал Task1Korolev

--- Client.js
+++ Client.js
@@ -1,6 +1,6 @@
 const rpc = require('json-rpc2')
 const client = rpc.Client.$create(8000, 'localhost')
 
-client.call('basicTasks.task1', ["Жене", 10], function (err, result) {
+client.call('tasks.task1Korolev', ["Андрею", 31], function (err, result) {
     console.log('Результат: ' + result)
 })
