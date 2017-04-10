ecbe0bbb22ad0d3a906ca8f7f50123421097566e
Kirill Keker <k.keker@globus-ltd.com>
2017-04-10 22:52:09 +0300

    Выполненное задание 1

--- Client.js
+++ Client.js
@@ -1,6 +1,6 @@
 const rpc = require('json-rpc2')
 const client = rpc.Client.$create(8000, 'localhost')
 
-client.call('add', [1, 2], function (err, result) {
-    console.log('1 + 2 = ' + result)
+client.call('basicTasks.task1', ["Жене", 10], function (err, result) {
+    console.log('Результат: ' + result)
 })
--- Server.js
+++ Server.js
@@ -11,7 +11,22 @@ function add(args, opt, callback) {
     callback(null, args[0] + args[1])
 }
 
-server.expose('add', add)
+function task1(args, opt, callback) {
+    callback(null, args[0] + ' ' + args[1] + ' ' + 'лет')
+}
+
+function echo(args, opt, callback) {
+    callback(null, args)
+}
+
+server.expose('basicTasks', {
+        'add': add,
+        'task1': task1
+})
+server.expose('debug', {
+        'echo': echo
+})
+
 server.listen(8000, 'localhost')
 
 console.log("Server started on http://localhost:8000/")
