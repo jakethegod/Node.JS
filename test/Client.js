ed269da184f4a4e5c97140f090fb45a9ecfbfbd1
Kirill Keker <k.keker@globus-ltd.com>
2017-04-16 23:15:34 +0300

    Code refactoring

--- test/Client.js
+++ test/Client.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const rpc = require('json-rpc2');
-const Config = require('../Config.json');
+const Config = require('../Config');
 
 const client = rpc.Client.$create(Config.Web.ListenPort, Config.Web.ListenAddr);
 
 client.call('mail.send', {
-    "ToMail": "k.keker@me.com",
+    "toMail": "k.keker@me.com",
     "subjTest": "Test Message",
     "messTest": "Message"
 }, function (err, result) {
